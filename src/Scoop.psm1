414a2ff833901045a1a6565ed4c3706f0c0b267e
yi_Xu <yi_Xu@yixuju.cn>
2022-10-24 21:14:41 +0800

    feat: Add `SCOOP_REPO` configuration option (#25)

--- src/Scoop.psm1
+++ src/Scoop.psm1
@@ -9,13 +9,15 @@ function Install-Scoop {
     $f = Join-Path $env:USERPROFILE 'install.ps1'
     Invoke-WebRequest 'https://raw.githubusercontent.com/ScoopInstaller/Install/master/install.ps1' -UseBasicParsing -OutFile $f
     & $f -RunAsAdmin
+    if ($env:SCOOP_REPO) {
+        Write-Log "Switching to repository: ${env:SCOOP_REPO}"
+        scoop config scoop_repo $env:SCOOP_REPO
+    }
     if ($env:SCOOP_BRANCH) {
         Write-Log "Switching to branch: ${env:SCOOP_BRANCH}"
-        scoop config SCOOP_BRANCH $env:SCOOP_BRANCH
-        scoop update
-    } else {
-        scoop update
+        scoop config scoop_branch $env:SCOOP_BRANCH
     }
+    scoop update
 }
 
 Export-ModuleMember -Function Install-Scoop
