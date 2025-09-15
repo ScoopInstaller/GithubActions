669b96d8244175690bfca499b63e7f0b49c1c655
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-09-15 20:36:42 +0100

    feat: add support for repository_dispatch event

--- src/ActionWrapper.psm1
+++ src/ActionWrapper.psm1
@@ -12,6 +12,7 @@ function Invoke-Action {
         'issue_comment' { Initialize-PR }
         'schedule' { Initialize-Scheduled }
         'workflow_dispatch' { Initialize-Scheduled }
+        'repository_dispatch' { Initialize-Scheduled }
         'issues' { Initialize-Issue }
         default { Write-Log 'Not supported event type' }
     }
