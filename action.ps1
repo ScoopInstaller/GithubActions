8720dc58b31abbfe1f0b9c21f5fff6ba6dd96dd3
Chawye Hsu <su+git@chawyehsu.com>
2026-08-21 00:43:36 +0800

    refactor: use irm for scoop install (#91)
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- action.ps1
+++ action.ps1
@@ -10,7 +10,6 @@ Install-Scoop
 Test-NestedBucket
 Initialize-NeededConfiguration
 
-git config --get user.email
 Write-LogInfo 'Importing all modules'
 # Load all scoop's modules.
 # Dot sourcing needs to be done on highest scope possible to propagate into lower scopes
